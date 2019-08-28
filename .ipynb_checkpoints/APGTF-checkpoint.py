5963bad6225532d109a79e134f8771fd9216ab09
st <bh_song@163.com>
2019-08-28 16:28:32 +0800

    add phase compensation control, allow to specify the delay after alignment update readme

--- .ipynb_checkpoints/APGTF-checkpoint.py
+++ .ipynb_checkpoints/APGTF-checkpoint.py
@@ -175,11 +175,11 @@ class APGTF:
         
         if is_plot:
             fig = plt.figure(figsize=[8,3],dpi=100)
-            axes = fig.subplots(1,2)
-            axes[0].semilogy(self.cfs,delays); 
-            axes[0].set_xlabel('Frquency(Hz)'); axes[0].set_ylabel('Delay(ms)');
-            axes[1].plot(self.cfs,gains); 
-            axes[1].set_xlabel('Frquency(Hz)'); axes[1].set_ylabel('Gain(dB)');
+            axs = fig.subplots(1,2)
+            axs[0].plot(self.cfs,delays); 
+            axs[0].set_xlabel('Frquency(Hz)'); axs[0].set_ylabel('Delay(ms)');
+            axs[1].plot(self.cfs,gains); 
+            axs[1].set_xlabel('Frquency(Hz)'); axs[1].set_ylabel('Gain(dB)');
             plt.tight_layout()
             
         return [delays,gains]
@@ -209,16 +209,16 @@ class APGTF:
         if fig is None:
             fig = plt.figure(figsize=[8,3],dpi=100)
         
-        axes = fig.subplots(1,2)
-        axes[0].plot(time_ticks,ir,linewidth=2); axes[0].set_xlim([self.low_cf*1.5,self.high_cf*1.5])
-        axes[0].set_xlabel('Time(s)'); axes[0].set_title(title)
+        axs = fig.subplots(1,2)
+        axs[0].plot(time_ticks,ir,linewidth=2); axs[0].set_xlim([self.low_cf*1.5,self.high_cf*1.5])
+        axs[0].set_xlabel('Time(s)'); axs[0].set_title(title)
         x_lim_max = time_ticks[np.max(np.nonzero(np.max(ir,axis=1)>np.max(ir)*1e-5)[0])]
-        axes[0].set_xlim([0,x_lim_max]) 
+        axs[0].set_xlim([0,x_lim_max]) 
         
-        axes[1].plot(freq_ticks,spec,linewidth=2); axes[1].set_xlim([self.low_cf*1.5,self.high_cf*1.5])
-        axes[1].set_xlabel('Frequency(Hz)'); axes[1].set_title('spectrum')
+        axs[1].plot(freq_ticks,spec,linewidth=2); axs[1].set_xlim([self.low_cf*1.5,self.high_cf*1.5])
+        axs[1].set_xlabel('Frequency(Hz)'); axs[1].set_title('spectrum')
         plt.tight_layout()
-        return fig
+        
         
     def plot_gtf(self,ir_duration=1,fig=None):
         """plot the impulse response and spectrum of gammatone filters
@@ -230,8 +230,7 @@ class APGTF:
         ir_len = np.int(self.fs*ir_duration)
         # impulse stimuli
         x = np.zeros((ir_len,1))
-        x[400] = 1# the spike
-        
+        x[100] = 1# the spike
         
         # gain and delay at center frequency before aligments and gain-normalized
         self.cal_delay_gain(is_plot=True)
@@ -239,22 +238,10 @@ class APGTF:
         # impulse response 
         # not aligned
         ir = self.filter_x(x,is_aligned=0);
-        fig = self.plot_ir_spec(ir.T,fig=None,title='IR(not aligned)') 
-        axes = fig.get_axes()
-        axes[0].plot(np.arange(ir_len)/self.fs,x,linewidth=2,label='impulse')
-        axes[0].set_xlim([0,0.06]); axes[0].set_ylim([-0.05,0.05])
-        axes[0].set_xlabel('time(s)'); axes[0].set_ylabel('amp')
-        axes[0].legend()
-        axes[0].set_title('impulse stimulus')
+        self.plot_ir_spec(ir.T,fig=fig,title='IR(not aligned)') 
 #         plt.savefig('example/ir_not_aligned.png')
         # aligned
         ir_algined = self.filter_x(x,is_aligned=1,aligned_delay=0);
-        fig = self.plot_ir_spec(ir_algined.T,fig=None,title='IR(aligned)') 
-        axes = fig.get_axes()
-        axes[0].plot(np.arange(ir_len)/self.fs,x,linewidth=2,label='impulse')
-        axes[0].set_xlim([0,0.06]); axes[0].set_ylim([-0.05,0.05])
-        axes[0].set_xlabel('time(s)'); axes[0].set_ylabel('amp')
-        axes[0].legend()
-        axes[0].set_title('impulse stimulus')
+        self.plot_ir_spec(ir_algined.T,fig=fig,title='IR(aligned)') 
 #         plt.savefig('example/ir_aligned.png')
         return [ir,ir_algined]
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Gammatone-filters
-Python implementation of all-pole Gammatone filter. 
+Python implementation of all-pole Gammatone filter.
 The filtering part of code is written in C.
 
 ## Basic ideas [^Holdsworth1988]
@@ -8,17 +8,21 @@ Gammatone filter can be regarded as lowpass filter with frequency shitfted by fc
 Algorithm details, see [README.pdf](README.pdf), currently written in Chinese,but most part are math equations.
 
 ## Impulse response of Gammatone filters
-![ir_not_aligned](example/ir_not_aligned.png)
+![ir_not_aligned](images/2019/08/ir-not-aligned.png)
 
 delay and gain at center frequency
-![delay_gain.png](example/delay_gain.png)
-basically,the phase delay at center frequency approximates 0. 
+
+![delay_gain](images/2019/08/delay-gain.png)
+
+basically,the phase delay at center frequency approximates 0.
 
 ### Phase compensation
 Phase compensatio is actually to align the peaks of all filter impulse response[^Brown1994], sincee the phase delay at center frequency is already approximate 0.
-![ir_aligned](example/ir_aligned.png)
 
-Next, I want to make summary about signal recovery after filtered by Gammatone filters. ![flag](example/flag.png)
+Align peaks of impulse responses to the maximum delay
+![ir_aligned](images/2019/08/ir-aligned.png)
+
+Next, I want to make summary about signal recovery after filtered by Gammatone filters. \[Flag\]
 
 
 
